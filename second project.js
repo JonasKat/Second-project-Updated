db64b773411d7cad6a34407050b1770bdf208262
Jonas Katauskis <katauskisjonas@gmail.com>
2016-01-18 21:55:57 +0200

    Try out of JSON using public API
    Background color changes accordingly to the www.colourlovers.com top
    color list.

--- second project.js
+++ second project.js
@@ -2,12 +2,13 @@
 $(document).ready(function() {
 	$('.box').on('click', function () {
 		var animations = [ 'crawl', 'dangle', 'jump', 'bounceOut', 'tada', 'shake'];
+        var $box = $(this);
 		var animation = animations[Math.floor(Math.random() * 6)];
 
-		$(this).addClass(animation);
+		$box.addClass(animation);
 
 		setTimeout(function () {
-    	$(".box").removeClass(animation);
+    	$box.removeClass(animation);
 		}, 4000);
 	});
     $(".fourth").on("click", function() {
@@ -18,4 +19,87 @@ $(document).ready(function() {
 			$(this).animate({"top": "0"});
 		}
     });
+    
+    //Try out of JSON
+    function randomColor1 () {
+        $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?",
+        { numResults: 3 },
+        function(colorPalettes) {
+        printColors(colorPalettes);
+        });
+        function printColors(colorPalettes){
+            for(var palette in colorPalettes){
+            console.log(colorPalettes[palette].title);
+            console.log(colorPalettes[palette].colors[0]); //HEX color code
+            };
+            $("body").css({"background": "#"+colorPalettes[palette].colors[0]});
+        };	
+    }
+    function randomColor2 () {
+        $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?",
+        { numResults: 5 },
+        function(colorPalettes) {
+        printColors(colorPalettes);
+        });
+        function printColors(colorPalettes){
+            for(var palette in colorPalettes){
+            console.log(colorPalettes[palette].title);
+            console.log(colorPalettes[palette].colors[0]); //HEX color code
+            };
+            $("body").css({"background": "#"+colorPalettes[palette].colors[1]});
+        };	
+    }
+    function randomColor3 () {
+        $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?",
+        { numResults: 5 },
+        function(colorPalettes) {
+        printColors(colorPalettes);
+        });
+        function printColors(colorPalettes){
+            for(var palette in colorPalettes){
+            console.log(colorPalettes[palette].title);
+            console.log(colorPalettes[palette].colors[0]); //HEX color code
+            };
+            $("body").css({"background": "#"+colorPalettes[palette].colors[2]});
+        };	
+    }
+    
+    function randomColor4 () {
+        $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?",
+        { numResults: 5 },
+        function(colorPalettes) {
+        printColors(colorPalettes);
+        });
+        function printColors(colorPalettes){
+            for(var palette in colorPalettes){
+            console.log(colorPalettes[palette].title);
+            console.log(colorPalettes[palette].colors[0]); //HEX color code
+            };
+            $("body").css({"background": "#"+colorPalettes[palette].colors[3]});
+        };	
+    }
+       function randomColor5 () {
+        $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?",
+        { numResults: 5 },
+        function(colorPalettes) {
+        printColors(colorPalettes);
+        });
+        function printColors(colorPalettes){
+            for(var palette in colorPalettes){
+            console.log(colorPalettes[palette].title);
+            console.log(colorPalettes[palette].colors[0]); //HEX color code
+            };
+            var colorEffect = ({"background": "#"+colorPalettes[palette].colors[4]});
+            $("body").css(colorEffect);
+            
+        };	
+    }
+    $(".first").on("click", randomColor1);
+    $(".third").on("click", randomColor2);  
+ 	$(".sixth").on("click", randomColor3);
+    $(".eigth").on("click", randomColor4); 
+    $(".tenth").on("click", randomColor5);
+     
+
+
 });
